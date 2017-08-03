eeb2e540d72a799bfd5039b976bc82ae5f1f247a
ankprogit <ankpro80@gmail.com>
2017-08-03 15:03:44 +0530

    tes

--- class-woocommerce-other-payment-gateway.php
+++ class-woocommerce-other-payment-gateway.php
@@ -270,7 +270,9 @@ class WC_Billecta_Payment_Gateway extends WC_Payment_Gateway{
 		
 		$order = new WC_Order( $order_id );
 $items = $order->get_items();
-
+	$order1 = wc_get_order( $order_id );
+		$order_data = $order1->get_data();
+		
 function getall_products($url,$request)
     {
        $authentication = base64_encode("dag@kreativinsikt.se:Stockholm66");
@@ -307,7 +309,6 @@ function getall_products($url,$request)
 	
 	
 //Creating the product in the billecta
-
 function creating_products($url,$request)
     {
        $authentication = base64_encode("dag@kreativinsikt.se:Stockholm66");
@@ -351,11 +352,9 @@ $product_public=array();
     $product_quantity = $item['quantity'];
  $product_total = $item['total'];
  $product_subtotal_tax = $item['subtotal_tax'];
-
  
 	 
   $product_variation_id = $item['variation_id'];
-
 	$product_request=array("CreditorPublicId"=>$CreditorPublicId,
 	"ArticleNumber"=> $product_id,
   "ProductExternalId"=> $product_id,
@@ -375,16 +374,62 @@ $product_public=array();
  $jsonData_allproduct=array();
  $jsonData_allproduct=json_encode($jsonData_allproduct);
 $all_pro=getall_products($get_allproduct,$jsonData_allproduct);
-
 $all_products=json_decode($all_pro);
+
+// Get all the customer
+ $get_allcustomer="https://apitest.billecta.com/v1/debtors/debtors/$CreditorPublicId";
+ $jsonData_allcustomer=array();
+ $jsonData_allcustomer=json_encode($jsonData_allcustomer);
+$all_cust=getall_products($get_allcustomer,$jsonData_allcustomer);
+$all_custmer=json_decode($all_cust);
+print_r($all_custmer);
+$cust_me=array();
+$cust_email=$order_data['billing']['email'];
+foreach($all_custmer as $cust)
+	{
+$cust_me[]=$cust->Email;
+
+	}
+	foreach($all_custmer as $cust)
+	{
+	if(!in_array($cust_email,$cust_me))
+	{
+	
+$url="https://apitest.billecta.com/v1/debtors/debtor";
+$request=array( 
+  "CreditorPublicId"=> $CreditorPublicId,
+  "Name"=> $order_data['billing']['first_name'].' '.$order_data['billing']['last_name'],
+  "Address"=>$order_data['billing']['address_1'].' '.$order_data['billing']['address_2'],
+  "ZipCode"=>$order_data['billing']['postcode'],
+  "City"=> $order_data['billing']['city'],
+  "CountryCode"=> $order_data['billing']['country'],
+  "Email"=>$order_data['billing']['email'],
+  "ContactName"=> $order_data['billing']['first_name'],
+  "ContactEmail"=> $order_data['billing']['email']);
+  $jsonDataEncoded = json_encode($request);
+ 
+  
+$output=CurlSendPostRequest($url,$jsonDataEncoded);
+$Outcome=json_decode($output);
+echo $publicIdcustomer=$Outcome['PublicId'];
+
+
+	//$product_public[]=$Outcome;
+	}else{
+	$product_public[]=$prod->DebtorPublicId;
+	$pro_descr=$prod->Description;
+		
+	}
+	}
+	
+	// eND OF THE CUSTOMER CHECKING
+exit;
 //print_r($all_products);
 $pro_me=array();
 foreach($all_products as $prod)
 	{
 $pro_me[]=$prod->ProductExternalId;
 	}
-
-
 if(!in_array($product_id,$pro_me))
 	{
 	
@@ -394,7 +439,6 @@ $jsonData_product = json_encode($product_request);
   
 $output1=creating_products($product_url,$jsonData_product);
  
-
 $obj=json_decode($output1,true);
 print_r($obj);
 $product_public[]=$output1;	
@@ -405,13 +449,11 @@ $product_public[]=$output1;
 		
 	}
 	
-
  
 }
 print_r($product_public['0']);
  
-		$order1 = wc_get_order( $order_id );
-		$order_data = $order1->get_data();
+	
 		//echo $order_data['currency'];
 		//print_r($order_data);
 	
@@ -485,26 +527,7 @@ print_r($product_public['0']);
         return $data;
     }
 	//	print_r($order_data['billing']);
-	$url="https://apitest.billecta.com/v1/debtors/debtor";
-$request=array( 
-  "CreditorPublicId"=> $CreditorPublicId,
-  "Name"=> $order_data['billing']['first_name'].' '.$order_data['billing']['last_name'],
-  "Address"=>$order_data['billing']['address_1'].' '.$order_data['billing']['address_2'],
-  "ZipCode"=>$order_data['billing']['postcode'],
-  "City"=> $order_data['billing']['city'],
-  "CountryCode"=> $order_data['billing']['country'],
-  "Email"=>$order_data['billing']['email'],
-  "ContactName"=> $order_data['billing']['first_name'],
-  "ContactEmail"=> $order_data['billing']['email']);
-  $jsonDataEncoded = json_encode($request);
- 
-  
-$output=CurlSendPostRequest($url,$jsonDataEncoded);
-$Outcome=json_decode($output);
-echo $publicIdcustomer=$Outcome['PublicId'];
-
-
-
+	
 //Creating the first invoice
 $current_date=date("Y-m-d h:i:s");
 $NewDate=date('Y-m-d h:i:s', strtotime("+3 days"));
@@ -524,7 +547,6 @@ $request=array(
 $output=CurlSendPostRequest($url,$jsonDataEncoded);
 $Outcome=json_decode($output);
 echo $publicIdcustomer=$Outcome['PublicId'];
-
 //end invoice
 	exit;
 		// Mark as on-hold (we're awaiting the cheque)
